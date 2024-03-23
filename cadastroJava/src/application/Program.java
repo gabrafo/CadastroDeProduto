3ac564fe446b91dcbd2d443aa3aa5fdfd69ae3dc
Gabriel Fagundes <113377700+gabrafo@users.noreply.github.com>
2024-03-23 01:34:55 -0300

    Add files via upload

---
+++ cadastroJava/src/application/Program.java
@@ -0,0 +1,65 @@
+package application;
+
+import entities.Product;
+
+import java.util.*;
+
+public class Program {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Sistema de cadastro de produtos");
+        System.out.println("-------------------------------");
+        List<Product> products = new ArrayList<>();
+        boolean addAnotherProduct = true;
+        
+        while(addAnotherProduct){
+        System.out.print("\nInsira o nome do produto: ");
+        String name = sc.nextLine();
+        System.out.print("Insira a descrição do produto: ");
+        String description = sc.nextLine();
+        System.out.print("Insira o preço do produto: R$");
+        Double price = sc.nextDouble();
+        System.out.print("O produto está disponível no estoque?(S/N) ");
+        boolean isAvailable = checkAnswer(sc);
+        products.add(new Product(name, description, price, isAvailable));
+
+        products.sort(Comparator.comparingDouble(Product::getPrice));
+        // OBS: Pensei em criar um método para ordenar usando Shell Sort, Quick Sort ou outro método de ordenação,
+        // mas o Java tem uma biblioteca que já faz isso sem a necessidade desse método. Portanto, vou deixar pra
+        // comprovar esses conhecimentos na solução em C++.
+
+        System.out.println("\nLista de Produtos:");
+        for (Product product : products) {
+            System.out.println(product.getName() + " - R$" + product.getPrice());
+        }
+
+        System.out.print("Você deseja adicionar um novo produto?(S/N) ");
+        addAnotherProduct = checkAnswer(sc);
+        }
+
+        sc.close();
+    }
+
+    public static boolean checkAnswer(Scanner sc) {
+        boolean validAnswer = false;
+        while (!validAnswer) {
+            try {
+                char resp = sc.next().charAt(0);
+                sc.nextLine(); // Limpa o buffer do scanner
+                if (resp == 's' || resp == 'S') {
+                    validAnswer = true;
+                    return true;
+                } else if (resp == 'n' || resp == 'N') {
+                    validAnswer = true;
+                    return false;
+                } else {
+                    System.out.println("Resposta inválida. Digite 'S' para sim ou 'N' para não.");
+                }
+            } catch (Exception e) {
+                System.out.println("Resposta inválida.");
+                sc.nextLine();
+            }
+        }
+        return false;
+    }
+}
